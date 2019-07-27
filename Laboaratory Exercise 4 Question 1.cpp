9ccad43284592a82c736193481870ed769b0824d
armandobautista20 <53230810+armandobautista20@users.noreply.github.com>
2019-07-27 20:39:42 +0300

    Add files via upload

---
+++ Laboaratory Exercise 4 Question 1.cpp
@@ -0,0 +1,26 @@
+
+
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include "_pause.h"
+
+using namespace std;
+
+
+int main() {
+    // ************************** TO DO **************************
+    // 1.	Write a program that accepts user’s section, and display
+    // them back with the format “*** Section: user’s section ***”.
+    // ***********************************************************
+
+   string user="";
+
+    cout << "Enter Section" << endl;
+    cin >> user;
+
+    cout <<"***Section: " << user <<"***" << endl;
+
+    system ("pause");
+    return 0;
+}
